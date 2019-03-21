c1d2115199dd160eb15ba61eda35bcbbe8f64439
Emmanuel idehen <23498282+emmanuelidehen@users.noreply.github.com>
2019-03-21 03:40:27 -0500

    Add files via upload

--- ViewController.swift
+++ ViewController.swift
@@ -1,15 +1,141 @@
+////
+////  ViewController.swift
+////  onbaording
+////
+////  Created by Emmanuel Idehen on 3/21/19.
+////  Copyright © 2019 Marquavious Draggon. All rights reserved.
+////
 //
-//  ViewController.swift
-//  onbaording
+//import UIKit
+//import AVFoundation
 //
-//  Created by Emmanuel Idehen on 3/21/19.
-//  Copyright © 2019 Marquavious Draggon. All rights reserved.
 //
+//class CameraView: UIView {
+//    override class var layerClass: AnyClass {
+//        get {
+//            return AVCaptureVideoPreviewLayer.self
+//        }
+//    }
+//    override var layer: AVCaptureVideoPreviewLayer {
+//        get {
+//            return super.layer as! AVCaptureVideoPreviewLayer
+//        }
+//    }
+//}
+//
+//
+////=========================================================================================================//
+//let session = AVCaptureSession()
+//let sessionQueue = DispatchQueue(label: AVCaptureSession.self.description(), attributes: [], target: nil)
+////========================================================================================================//
+//var cameraView: CameraView!
+//
+//class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+//
+//
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        // Do any additional setup after loading the view.
+//
+//     //===============================================================================================================//
+//        session.beginConfiguration()
+//        let videoDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+//        if (videoDevice != nil) {
+//            let videoDeviceInput = try? AVCaptureDeviceInput(device: videoDevice)
+//            if (videoDeviceInput != nil) {
+//                if (session.canAddInput(videoDeviceInput)) {
+//                    session.addInput(videoDeviceInput)
+//                }
+//            }
+//            let metadataOutput = AVCaptureMetadataOutput()
+//            if (session.canAddOutput(metadataOutput)) {
+//                session.addOutput(metadataOutput)
+//                metadataOutput.metadataObjectTypes = [
+//                    AVMetadataObject.ObjectType.ean13,
+//                    AVMetadataObject.ObjectType.qr
+//                ]
+//                metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+//            }
+//        }
+//        session.commitConfiguration()
+//        cameraView.layer.session = session
+//        cameraView.layer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+//
+//        //=============================================================================================//
+//        let videoOrientation: AVCaptureVideoOrientation
+//        switch UIApplication.shared.statusBarOrientation {
+//        case .portrait:
+//            videoOrientation = .portrait
+//        case .portraitUpsideDown:
+//            videoOrientation = .portraitUpsideDown
+//        case .landscapeLeft:
+//            videoOrientation = .landscapeLeft
+//        case .landscapeRight:
+//            videoOrientation = .landscapeRight
+//        default:
+//            videoOrientation = .portrait
+//        }
+//        cameraView.layer.connection?.videoOrientation = videoOrientation
+//       //=================================================================================================//
+//
+//    }
+//
+//    //====Overide Viewdidload====//
+//
+//    override func loadView() {
+//        cameraView = CameraView()
+//        view = cameraView
+//    }
+//
+//  //==============================================================
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//        sessionQueue.async {
+//            self.session.startRunning()
+//        }
+//    }
+//    override func viewWillDisappear(_ animated: Bool) {
+//        super.viewWillDisappear(animated)
+//        sessionQueue.async {
+//            self.session.stopRunning()
+//        }
+//    }
+//
+//  //==========================================================
+//    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+//        super.viewWillTransition(to: size, with: coordinator)
+//        // Update camera orientation
+//        let videoOrientation: AVCaptureVideoOrientation
+//        switch UIDevice.current.orientation {
+//        case .portrait:
+//            videoOrientation = .portrait
+//        case .portraitUpsideDown:
+//            videoOrientation = .portraitUpsideDown
+//        case .landscapeLeft:
+//            videoOrientation = .landscapeRight
+//        case .landscapeRight:
+//            videoOrientation = .landscapeLeft
+//        default:
+//            videoOrientation = .portrait
+//        }
+//        cameraView.layer.connection?.videoOrientation = videoOrientation
+//    }
+//    /*
+//    // MARK: - Navigation
+//
+//    // In a storyboard-based application, you will often want to do a little preparation before navigation
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        // Get the new view controller using segue.destination.
+//        // Pass the selected object to the new view controller.
+//    }
+//    */
+//
+//}
 
 import UIKit
 import AVFoundation
-
-
 class CameraView: UIView {
     override class var layerClass: AnyClass {
         get {
@@ -22,29 +148,106 @@ class CameraView: UIView {
         }
     }
 }
-
-
-
-
-var cameraView: CameraView!
-
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    // Camera view
+    var cameraView: CameraView!
+    // AV capture session and dispatch queue
+    let session = AVCaptureSession()
+    let sessionQueue = DispatchQueue(label: AVCaptureSession.self.description(), attributes: [], target: nil)
+    override func loadView() {
+        cameraView = CameraView()
+        view = cameraView
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        session.beginConfiguration()
+        
+        
+//        AVCaptureDevice.default(for: AVMediaType.video)
+//        {
+//            let input = AVCaptureDeviceInput(device: captureDevice)
+//            session.addInput(input)
+//        }
+        
+        
+     //   let videoDevice = AVCaptureDevice.default(for: AVMediaType.video)
+        let videoDevice = AVCaptureDevice.default(for: AVMediaType.video)
+        
+        if (videoDevice != nil) {
+            let videoDeviceInput = try? AVCaptureDeviceInput(device: videoDevice!)
+            if (videoDeviceInput != nil) {
+                if (session.canAddInput(videoDeviceInput!)) {
+                    session.addInput(videoDeviceInput!)
+                }
+            }
+            let metadataOutput = AVCaptureMetadataOutput()
+            if (session.canAddOutput(metadataOutput)) {
+                session.addOutput(metadataOutput)
+                metadataOutput.metadataObjectTypes = [
+                    AVMetadataObject.ObjectType.ean13,
+                    AVMetadataObject.ObjectType.qr
+                ]
+                metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            }
+        }
+        session.commitConfiguration()
+        cameraView.layer.session = session
+        cameraView.layer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        // Set initial camera orientation
+        let videoOrientation: AVCaptureVideoOrientation
+        switch UIApplication.shared.statusBarOrientation {
+        case .portrait:
+            videoOrientation = .portrait
+        case .portraitUpsideDown:
+            videoOrientation = .portraitUpsideDown
+        case .landscapeLeft:
+            videoOrientation = .landscapeLeft
+        case .landscapeRight:
+            videoOrientation = .landscapeRight
+        default:
+            videoOrientation = .portrait
+        }
+        cameraView.layer.connection?.videoOrientation = videoOrientation
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // Start AV capture session
+        sessionQueue.async {
+            self.session.startRunning()
+        }
     }
-    */
-
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        // Stop AV capture session
+        sessionQueue.async {
+            self.session.stopRunning()
+        }
+    }
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        // Update camera orientation
+        let videoOrientation: AVCaptureVideoOrientation
+        switch UIDevice.current.orientation {
+        case .portrait:
+            videoOrientation = .portrait
+        case .portraitUpsideDown:
+            videoOrientation = .portraitUpsideDown
+        case .landscapeLeft:
+            videoOrientation = .landscapeRight
+        case .landscapeRight:
+            videoOrientation = .landscapeLeft
+        default:
+            videoOrientation = .portrait
+        }
+        cameraView.layer.connection?.videoOrientation = videoOrientation
+    }
+    func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [Any]!, from connection: AVCaptureConnection!) {
+        // Display barcode value
+        if (metadataObjects.count > 0 && metadataObjects.first is AVMetadataMachineReadableCodeObject) {
+            let scan = metadataObjects.first as! AVMetadataMachineReadableCodeObject
+            let alertController = UIAlertController(title: "Barcode Scanned", message: scan.stringValue, preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: .default, handler:nil))
+            present(alertController, animated: true, completion: nil)
+        }
+    }
 }
